4e3e3c314c47291b83cc37fadc663f9b5c8487fa
kiku-hub <tacyskani@gmail.com>
2024-10-09 14:54:32 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,7 @@
 class MessagesController < ApplicationController
-
   def index
      @message = Message.new
      @room = Room.find(params[:room_id])
-     @messages = @room.messages.includes(:user)
    end
  
    def create
@@ -12,10 +10,10 @@ class MessagesController < ApplicationController
      if @message.save
        redirect_to room_messages_path(@room)
      else
-       @messages = @room.messages.includes(:user)
        render :index, status: :unprocessable_entity
      end
    end
+ 
  
    private
  
@@ -23,5 +21,8 @@ class MessagesController < ApplicationController
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
  end
+ 
+
+
  
 
